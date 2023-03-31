67fe83c9a3b27c4cfbea341691945d1c611be75f
Bea <116292796+BeaNoodle@users.noreply.github.com>
2023-03-31 13:08:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}    
+
+function mediaList() {
+    document.getElementById("mediaDropdown").classList.toggle("show");
+}
+
+function umineko() {
+    document.getElementById("uminekoDropdown").classList.toggle("show");
+}
